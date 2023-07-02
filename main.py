fb7b737c6ea8b2738be166c23ff725f5022c3331
Adam <schoferstd@gmail.com>
2023-07-02 16:16:48 +0200

    Kick and ban commands, weather in embed

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import requests
 import json
 from discord.ext.commands import has_permissions, MissingPermissions
 import os
+import random
+from tabs import *
 
 intents = discord.Intents.all()
 intents.members = True
@@ -19,18 +21,26 @@ def kelvin_to_celsius(kelvin):
     celsius = kelvin - 273.15
     return celsius
 
+def random_message(tab):
+    a = 0
+    b = len(tab) - 1
+    rand = random.randint(a,b)
+    return tab[rand]
+
+
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Activity(type=discord.ActivityType.playing, name = random_message(activities)))
     print("Hello! I'm AiOBot. I'm now ready for use!")
     print("-----------------------------------------")
 
 @client.command()
 async def hello(ctx):
-    await ctx.send("Hello, I am AiOBot. How can I help?")
+    await ctx.send(random_message(hello_messages))
 
 @client.command()
 async def goodbye(ctx):
-    await ctx.send("Goodbye, sweet dreams!")
+    await ctx.send(random_message(goodbye_message))
 
 @client.command()
 async def username(ctx):
@@ -39,12 +49,12 @@ async def username(ctx):
 @client.event
 async def on_member_join(member):
     channel = client.get_channel(JOIN_LEAVE_MESSAGE_CHANNEL)
-    await channel.send("Welcome")
+    await channel.send(ON_MEMBER_JOIN_MESSAGE)
 
 @client.event
 async def on_member_remove(member):
     channel = client.get_channel(JOIN_LEAVE_MESSAGE_CHANNEL)
-    await channel.send("Goodbye!")
+    await channel.send(ON_MEMBER_LEAVE_MESSAGE)
 
 @client.command()
 async def weather(ctx, city):
@@ -58,32 +68,39 @@ async def weather(ctx, city):
     sunrise_time = dt.datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
     sunset_time = dt.datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
     wind_speed = data['wind']['speed']
-    await ctx.send(f"Weather in {city} \nTemperature: {temp:.2f}°C \nFeels like temperature: {feels_temp:.2f}°C \nHumidity: {humidity}% \nWind speed: {wind_speed}m/s \nGeneral description: {desc} \nSunrise time: {sunrise_time} local time \nSunset time: {sunset_time} local_time")
+    embed = discord.Embed(title = f'Pogoda w {city}', description = '', color=0x00ffff)
+    embed.set_author(name="OpenWeather", url='https://openweathermap.org', icon_url = 'https://imgur.com/RvbHVqQ.png')
+    embed.set_thumbnail(url='https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/The_Weather_Channel_logo_2005-present.svg/225px-The_Weather_Channel_logo_2005-present.svg.png')
+    embed.add_field(name="Ogólna pogoda ", value=f'{desc}', inline=False)
+    embed.add_field(name="Temperatura: ", value=f'{temp:.2f}°C', inline=False)
+    embed.add_field(name="Temperatura odczuwalna: ", value=f'{feels_temp:.2f}°C', inline=False)
+    embed.add_field(name="Wilgotność: ", value=f'{humidity}%', inline=False)
+    embed.add_field(name="Prędkość wiatru: ", value=f'{wind_speed}m/s', inline=False)
+    embed.add_field(name="Wschód słońca: ", value=f'{sunrise_time} czasu lokalnego', inline=False)
+    embed.add_field(name="Zachód słońca: ", value=f'{sunset_time} czasu lokalnego', inline=False)
+    embed.set_footer(text="Informacje pogodowe dostarcza OpenWeather")
+    await ctx.send(embed = embed)
 
 @client.command()
 @has_permissions(kick_members = True)
 async def kick(ctx, member:discord.Member, *, reason=None):
     await member.kick(reason=reason)
-    await ctx.send(f'User {member} has been kicked')
+    await ctx.send(f'Użytkownik {member} otrzymał kikca z powody {reason}')
 
 @kick.error
 async def kick_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You don't have permissions to kick peoples from this server.")
+        await ctx.send("Nie masz uprawnień do dawania kicków na tym serwerze")
 
 @client.command()
 @has_permissions(ban_members = True)
 async def ban(ctx, member:discord.Member, *, reason=None):
     await member.ban(reason=reason)
-    await ctx.send(f'User {member} has been banned')
+    await ctx.send(f'Użytkownik {member} został zbanowany z powodu {reason}')
 
 @kick.error
 async def ban_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You don't have permissions to ban peoples from this server.")
-
-
-
-
+        await ctx.send("Nie masz uprawnień do banowania na tym serwerze")
 
 client.run(TOKEN)
---
+++ tabs.py
@@ -0,0 +1,5 @@
+activities = ['Wypasanie krówek na Marsie', 'Kupowanie kryptowalut', 'Przejmowanie internetu', 'Szukanie memów', 'Spanko']
+hello_messages = ['Witaj!', 'Cześć!', 'Dzień dobry.']
+goodbye_message = ['Żegnaj!', 'Do widzenia!', 'Pa!']
+ON_MEMBER_JOIN_MESSAGE = 'Właśnie ktoś dołączył do Nas! Przywitajmy go ciepło.'
+ON_MEMBER_LEAVE_MESSAGE = 'Smutne wieści Nas spotkały... Jeden z użytkowników uciekł z serwera'
